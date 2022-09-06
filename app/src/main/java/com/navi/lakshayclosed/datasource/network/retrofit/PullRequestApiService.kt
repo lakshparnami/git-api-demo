95f46e9bf43bfe5196628a6d874a5c050b9019db
Lakshay <lakshay@godutchpay.in>
2022-09-06 18:58:46 +0530

    remove github header

--- app/src/main/java/com/navi/lakshayclosed/datasource/network/retrofit/PullRequestApiService.kt
+++ app/src/main/java/com/navi/lakshayclosed/datasource/network/retrofit/PullRequestApiService.kt
@@ -7,7 +7,6 @@ import retrofit2.http.Query
 
 interface PullRequestApiService {
 
-    @Headers("Authorization:Bearer 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")
     @GET("/repos/square/retrofit/pulls")
     suspend fun getPullRequests(
         @Query("page") page: Int,
